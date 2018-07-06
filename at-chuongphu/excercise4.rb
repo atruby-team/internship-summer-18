9da5a820e0c514d2714d33e989c162c37f11db72
Chuong Phu <chuong.phu@asiantech.vn>
2018-07-06 09:57:56 +0700

    excercise-ruby-d4

---
+++ at-chuongphu/excercise4.rb
@@ -0,0 +1,20 @@
+# ex1
+array = 'asianstech'.split("") 
+array.each do |x|
+    puts array.index(x) if array.count(x) == 1
+# end    
+# # ex2
+def sumarr a
+ check = a.all?{|x| x.is_a? Integer}
+ if check == true
+	puts a.reduce(0,:+)
+ else
+	puts "nil"
+ end
+end
+a= [1,2,4,5]
+sumarr(a)
+#ex4
+human_name= "alan john"
+final = human_name.split.map(&:capitalize).join(' ')
+puts final
