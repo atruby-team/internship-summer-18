de20bce8f73ca535b2fdd0353b4e319ab4e97cda
at-luantran <luan.tran@asiantech.vn>
2018-07-06 09:54:19 +0700

    Exericse Ruby Day 3

---
+++ at-luantran/Week3/2018_07_05_exercise4_rubyday3.rb
@@ -0,0 +1,42 @@
+class StringFormat
+  class << self
+  def human_name(string)
+    if string.is_a? String
+      i = 0
+      out = ''
+      string.strip!
+      string.gsub!(/[^A-Za-z ]/, '')
+      string.each_char do |x|
+        if string[i - 1] == ' ' && x != ' ' || i.zero?
+          _a = x.ord - 32
+          out += _a.chr
+        else
+          out += x
+        end
+        i += 1
+      end
+      out
+    else
+      nil
+    end
+  end
+
+  def uniq(string)
+    hash = {}
+    string.each_char { |_str| hash[_str] = ((hash[_str] || 0) + 1) }
+    hash.keys.inject { |out, i| out + i }
+  end
+
+  def only_letter?(string)
+    if string.is_a? String
+      string.length <= 1
+    else
+      nil
+    end
+  end
+  end
+end
+
+puts StringFormat.human_name 'kaylene _johnson'
+puts StringFormat.uniq 'Hello word'
+puts StringFormat.only_letter? 'Hello'
