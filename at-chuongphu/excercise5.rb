b69e1e5bb1807ad981783ecd943c47fb097a3892
Chuong Phu <chuong.phu@asiantech.vn>
2018-07-09 10:06:52 +0700

    fix-white_space

--- at-chuongphu/excercise5.rb
+++ at-chuongphu/excercise5.rb
@@ -1,34 +1,44 @@
 # ex1
 class Student
-	attr_accessor :first_name, :last_name, :list_course
+  attr_accessor :first_name, :last_name, :list_course
   def initialize
     @list_course = []
   end
+
   def first_name
     puts "#{@first_name}"
   end
+
   def last_name
     puts "#{@last_name}"
   end
+
   def full_name
     puts "#{@first_name}" + " #{@last_name}"
   end
+
   def add_course(course_name)
-    # @list_course = []
-    @list_course.push(course_name) unless @list_course.include?(course_name)
-	end
-	def remove_course(course_name)
-		if @list_course.include?(course_name)
-			@list_course.delete(course_name)
-			return course_name
-		else
-			return false
-		end
-	end
+    # @list_course.push(course_name) unless @list_course.include?(course_name)
+    if @list_course.include?(course_name)
+      return false
+    else 
+      @list_course.push(course_name)
+  end
+  
+  def remove_course(course_name)
+    if @list_course.include?(course_name)
+      @list_course.delete(course_name)
+	  return course_name
+	else
+	  return false
+    end
+  end
+  
   def show
     @list_course
   end
 end
+
 student = Student.new
 student.first_name = "Alan"
 student.last_name = "Thompson"
