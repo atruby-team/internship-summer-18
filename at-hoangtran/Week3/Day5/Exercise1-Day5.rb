05f4e06df17501e088bc3c3e4edc5c65cad60bf0
HoangTran <hoang.tran@asiantech.vn>
2018-07-09 09:16:08 +0700

    Exercise Variables, Getter, Setter, Accessor

--- at-hoangtran/Week3/Day5/Exercise1-Day5.rb
+++ at-hoangtran/Week3/Day5/Exercise1-Day5.rb
@@ -1,25 +1,27 @@
 class Student
-  attr_accessor :first_name, :last_name
-  def get_set
-    puts 
+  attr_accessor :first_name, :last_name, :list_of_courses
+  def initialize
+    @list_of_courses = []
   end
+
   def full_name
-    puts "#{@first_name}" + " #{@last_name}"
+    puts @first_name.to_s + ' ' + @last_name.to_s
   end
+
   def add_course(course_name)
-    list_of_courses = Array.new
-    @course_name = course_name
-    if list_of_courses.include?("#{@course_name}") == false 
-      list_of_courses.push(@course_name)          
-    end    
-    puts list_of_courses
+    @list_of_courses.push(course_name) unless @list_of_courses.include?(course_name)
+  end
+
+  def remove_course(course_name)
+    @list_of_courses.delete(course_name) if @list_of_courses.include?(course_name)
   end
 end
-
 student = Student.new
+student.list_of_courses = ['math', 'history']
 student.first_name = 'Tran'
 student.last_name = 'Hoang'
-student.first_name
-student.full_name
-student.add_course('Math')
-student.add_course('Education')
+puts student.full_name
+puts student.first_name
+student.add_course('physical')
+student.remove_course('math')
+print student.list_of_courses
