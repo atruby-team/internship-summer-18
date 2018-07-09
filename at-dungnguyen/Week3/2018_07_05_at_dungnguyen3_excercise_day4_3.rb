f0d0660bf5f99a9a983d87c7561868b11394b606
at-dungnguyen3 <dung.nguyen3@asiantech.vn>
2018-07-09 10:16:49 +0700

    Exercise 3 - String to HTML

--- at-dungnguyen/Week3/2018_07_05_at_dungnguyen3_excercise_day4_3.rb
+++ at-dungnguyen/Week3/2018_07_05_at_dungnguyen3_excercise_day4_3.rb
@@ -4,7 +4,23 @@ class HtmlToText
   end
 
   def get_html(html)
-    html.gsub(%r{<\/?[div br]*>}, "\n")
+    flag = true
+    stack = []
+    len = html.length
+    0.upto(len) do |i|
+      html[i..i + 3] = "\n" if html[i..i + 3] == '<br>'
+      if html[i..i + 4] == '<div>'
+        stack.push(html[i..i + 4])
+        if flag == true
+          html[i..i + 4] = html[i..i + 4] == html[0..4] ? '' : "\n"
+        end
+      end
+      next unless html[i..i + 5] == '</div>'
+      html[i..i + 5] = flag == true && stack.pop == '<div>' ? "\n" : ''
+      flag = false
+      flag = true if html[i + 5..i + 9] != '<div>' || html[i + 5..i + 10] != '</div>'
+    end
+    html
   end
 
   def show_view
@@ -12,5 +28,5 @@ class HtmlToText
   end
 end
 
-a = HtmlToText.new('<div>Hello Ruby</div><<<br>>><div>Will</div> be</div>Fun<p></p>')
+a = HtmlToText.new('<div>Hello Ruby</div><<<br>>><div>Will</div> be</div>Fun<div><p></p>')
 a.show_view
