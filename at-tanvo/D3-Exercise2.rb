1f01468ceb2d0bd8005b69c9ef199c57ce0d1194
at-tanvo <tan.vo@asiantech.vn>
2018-07-09 09:29:56 +0700

    Exercise

--- at-tanvo/D3-Exercise2.rb
+++
@@ -1,4 +0,0 @@
-arr = [1, 2, 3, 1]
-puts arr.to_s
-arr.map! { |i| i.even? ? 'even' : 'odd' }
-puts arr.to_s
--- at-tanvo/Exercise2.rb
+++ at-tanvo/Exercise2.rb
@@ -1,23 +1,83 @@
-def add_more(num)
-  ->(i) { i + num }
+class Dictionary
+  def initialize
+    @dict = []
+    @word = []
+    @count = {}
+  end
+
+  def add(vocal, descr)
+    temp = {}
+    unless @dict.include?(vocal)
+      temp[vocal] = descr
+      @word.push(vocal)
+      @dict.push(temp)
+    end
+  end
+
+  def show
+    @dict
+  end
+
+  def look_up(vocal)
+    @dict.each do |n|
+      next unless n.key?(vocal)
+      @count[vocal] ? @count[vocal] += 1 : @count[vocal] = 1
+      return n[vocal]
+    end
+    'khong co ket qua'
+  end
+
+  def pop
+    temp = @word.last
+    @count[temp]
+    @word.pop
+    @dict.pop
+  end
+
+  def remove(vocal)
+    id = @dict.index { |dict| dict.key?(vocal) }
+    @dict.delete_at(id)
+    @word.delete(vocal)
+  end
+
+  def size
+    @dict.size
+  end
+
+  def update(vocal, descr)
+    if @dict.key?(vocal)
+      remove(vocal)
+      add(vocal, descr)
+    end
+  end
+
+  def random
+    @dict.sample
+  end
+
+  def favorite
+    array = @count.max_by { |_k, v| v }
+    @dict.each do |n|
+      return n if n.key?(array.first)
+    end
+  end
 end
 
-def multi_more(num)
-  ->(i) { i * num }
-end
+dic = Dictionary.new
+dic.add('Hi', 'Xin Chao')
+dic.add('go', 'di')
+dic.add('gender', 'Gioi tinh')
+dic.add('team', 'doi')
+dic.add('party', 'Buoi tiec')
+dic.add('truck', 'xe tai')
+dic.add('end', 'het')
+dic.add('over', 'ket thuc')
+puts dic.size
+puts dic.show
+dic.look_up('go')
+p dic.look_up('go')
+dic.pop
+p dic.show
+p dic.favorite
+p dic.random
 
-def compose(lambda1, lambda2)
-  ->(i) { lambda2.call(lambda1.call(i)) }
-end
-add4 = add_more(4)
-add10 = add_more(10)
-multi10 = multi_more(10)
-multi5 = multi_more(5)
-puts add4.call(5)
-puts add10.call(12)
-puts multi10.call(10)
-puts multi5.call(10)
-add4_and_multi10 = compose(add4, multi10)
-puts add4_and_multi10.call(10)
-multi10_and_add4 = compose(multi10, add4)
-puts multi10_and_add4.call(10)
