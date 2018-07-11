b6d589e429593db502fd621efd7899696a33e7ad
HoangTran <hoang.tran@asiantech.vn>
2018-07-11 08:29:20 +0700

    Exercise Block, Proc, Lambda

--- at-hoangtran/Week4/Day2/Exercise1-Day2.rb
+++ at-hoangtran/Week4/Day2/Exercise1-Day2.rb
@@ -1,4 +1,10 @@
-add = lambda{ |x, y| x + y }
-sub = lambda{ |x, y| x - y }
-multi = lambda{ |x, y| x * y }
-div = lambda{ |x, y| x / y }
+add = lambda { |*n| n.inject { |x, y| x + y } }
+sub = lambda { |*n| n.inject { |x, y| x - y } }
+multi = lambda { |*n| n.inject { |x, y| x * y } }
+div = lambda do |*n|
+  n.inject { |x, y| y.zero? ? 'can not div by zero' : x / y }
+end
+puts add.call(5, 7, 4, 2)
+puts sub.call(5, 7, 4, 2)
+puts multi.call(5, 7, 4, 2)
+puts div.call(9, 0)
