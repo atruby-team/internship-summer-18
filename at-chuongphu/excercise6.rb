3b5d6d2f57e93ef133c7920c22a8c55b5aa82c09
Chuong Phu <chuong.phu@asiantech.vn>
2018-07-11 09:39:05 +0700

    fix-call_method-v3

--- at-chuongphu/excercise6.rb
+++ at-chuongphu/excercise6.rb
@@ -5,10 +5,10 @@ nhan = proc { |*ar| ar.inject { |a, b| a * b } }
 chia = proc do |*ar|
   ar.inject { |a, b| a / b unless b.zero? }
 end
-p cong.(3, 1, 4)
-p tru.(7, 2)
-p nhan.(5, 2)
-p chia.(100, 5, 2)
+p cong.call(3, 1, 4)
+p tru.call(7, 2)
+p nhan.call(5, 2)
+p chia.call(100, 5, 2)
 # ex2
 def add_more(num)
   proc { |x| num + x }
