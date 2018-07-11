a14d9301bd8e4d0cbff46059c1d9e28bd058d0ca
Quan Nguyen H <quan.nguyen2@asiantech.vn>
2018-07-11 10:02:35 +0700

    W4 - D2 - QuanNguyenH - Block Proc Lambada

--- at-quannguyen/W4/ex1.rb
+++ at-quannguyen/W4/ex1.rb
@@ -1,5 +1,19 @@
-add = ->(a, b) { a + b }
-sub = ->(a, b) { a - b }
-multi = ->(a, b) { a * b }
-div = ->(a, b) { a / b }
+def operator
+  Proc.new
+end
 
+add = operator { |*arr| arr.inject(:+) }
+sub = operator { |*arr| arr.inject(:-) }
+mul = operator { |*arr| arr.inject(:*) }
+div = operator do |*arr|
+  begin
+    arr.inject(:/)
+  rescue StandardError
+    puts 'can not div by zero'
+  end
+end
+
+p add.call(2, 5, 6)
+p sub.call(2, 3)
+p mul.call(1, 2, 3)
+p div.call(1, 2, 0)
--- at-quannguyen/W4/ex3.rb
+++ at-quannguyen/W4/ex3.rb
@@ -1,5 +1,34 @@
 class ArrayClone
+  attr_accessor :array, :length
+  def initialize
+    self.array = []
+    self.length = 0
+  end
 
+  def map(&block)
+    result = []
+    @array.each { |e| result << yield(e) }
+    result
+  end
+
+  def select(&block)
+    result = []
+    @array.each { |e| result << i if yield(e) }
+    result
+  end
+
+  def a_length
+    @array.each { |_e| @length += 1 }
+    @length
+  end
 end
 
 arr = ArrayClone.new
+arr.array = [1, 2, 3, 5, 6]
+
+p arr.array.length
+new_arr = arr.map { |i| i + 1 }
+p new_arr
+p arr.select do |i|
+  i > 3
+end
