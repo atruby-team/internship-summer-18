23e5e7330194fbf153703876966298a5e1d4b63a
at-dungnguyen3 <dung.nguyen3@asiantech.vn>
2018-07-17 13:46:16 +0700

    Edit Name Sql String

--- at-dungnguyen/Week5/route.rb
+++ at-dungnguyen/Week5/route.rb
@@ -13,48 +13,48 @@ get '/posts' do
   total = client.query(total_posts).first['total']
 
   @total_pages = (total.to_f / row_count).ceil
-  sql = "SELECT * FROM posts ORDER BY id DESC LIMIT #{offset}, #{row_count}"
-  @posts = client.query(sql).to_a
+  top_10_post_newest = "SELECT * FROM posts ORDER BY id DESC LIMIT #{offset}, #{row_count}"
+  @posts = client.query(top_10_post_newest).to_a
   erb :index_posts
 end
 
 get '/posts/:id' do
-  sql = "SELECT * FROM posts WHERE id = #{params[:id]}"
-  sql2 = "SELECT * FROM comments WHERE post_id = #{params[:id]}"
-  @post = client.query(sql).first
-  @comments = client.query(sql2).to_a
-  sql3 = "SELECT * FROM users WHERE id = #{@post['user_id']}"
-  @user = client.query(sql3).first
+  post = "SELECT * FROM posts WHERE id = #{params[:id]}"
+  comments = "SELECT * FROM comments WHERE post_id = #{params[:id]}"
+  @post = client.query(post).first
+  @comments = client.query(comments).to_a
+  writer = "SELECT * FROM users WHERE id = #{@post['user_id']}"
+  @user = client.query(writer).first
   erb :detail_posts
 end
 
 post '/posts' do
-  sql = "INSERT INTO posts(content, user_id, comment_count) VALUES('#{params[:content]}', #{params[:user_id]}, 0)"
-  client.query(sql)
+  add_post = "INSERT INTO posts(content, user_id, comment_count) VALUES('#{params[:content]}', #{params[:user_id]}, 0)"
+  client.query(add_post)
 end
 
 delete '/posts/:id' do
-  sql = "DELETE FROM posts WHERE id = #{params[:id]}"
-  sql2 = "DELETE FROM comments WHERE post_id = #{params[:id]}"
-  client.query(sql)
-  client.query(sql2) if client.affected_rows > 0
+  del_post = "DELETE FROM posts WHERE id = #{params[:id]}"
+  del_comments = "DELETE FROM comments WHERE post_id = #{params[:id]}"
+  client.query(del_post)
+  client.query(del_comments) if client.affected_rows > 0
 end
 
 put '/posts/:id' do
-  sql = "UPDATE posts SET content = '#{params[:content]}' WHERE id = #{params[:id]}"
-  client.query(sql)
+  update_content = "UPDATE posts SET content = '#{params[:content]}' WHERE id = #{params[:id]}"
+  client.query(update_content)
 end
 
 post '/posts/:id/comments' do
-  sql = "INSERT INTO comments(content, post_id) VALUES('#{params[:content]}', #{params[:id]})"
-  client.query(sql)
-  sql2 = "UPDATE posts SET comment_count = comment_count + 1 WHERE id = #{params[:id]}"
-  client.query(sql2) if client.affected_rows > 0
+  add_comment = "INSERT INTO comments(content, post_id) VALUES('#{params[:content]}', #{params[:id]})"
+  client.query(add_comment)
+  update_count = "UPDATE posts SET comment_count = comment_count + 1 WHERE id = #{params[:id]}"
+  client.query(update_count) if client.affected_rows > 0
 end
 
 delete '/posts/:id/comments/:cmt_id' do
-  sql = "DELETE FROM comments WHERE id = #{params[:cmt_id]}"
-  client.query(sql)
-  sql2 = "UPDATE posts SET comment_count = comment_count - 1 WHERE id = #{params[:id]}"
-  client.query(sql2) if client.affected_rows > 0
+  del_comment = "DELETE FROM comments WHERE id = #{params[:cmt_id]}"
+  client.query(del_comment)
+  update_count = "UPDATE posts SET comment_count = comment_count - 1 WHERE id = #{params[:id]}"
+  client.query(update_count) if client.affected_rows > 0
 end
