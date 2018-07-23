834451464516aa55ed2fa56b4496badd74fc940e
Chuong Phu <chuong.phu@asiantech.vn>
2018-07-23 10:29:06 +0700

    fixv2-bugs

--- at-chuongphu/excercise5.rb
+++ at-chuongphu/excercise5.rb
@@ -6,21 +6,21 @@ class Student
   end
 
   def first_name
-    puts "#{@first_name}"
+    puts @first_name.to_s
   end
 
   def last_name
-    puts "#{@last_name}"
+    puts @last_name.to_s
   end
 
   def full_name
-    puts "#{@first_name}" + " #{@last_name}"
+    puts @first_name.to_s + " #{@last_name}"
   end
 
   def add_course(course_name)
     if @list_course.include?(course_name)
-      return false
-    else 
+      false
+    else
       @list_course.push(course_name)
     end
   end
@@ -28,24 +28,24 @@ class Student
   def remove_course(course_name)
     if @list_course.include?(course_name)
       @list_course.delete(course_name)
-	    return course_name
-	  else
-	    return false
+      course_name
+    else
+      false
     end
   end
-  
+
   def show
     @list_course
   end
 end
 
 student = Student.new
-student.first_name = "Alan"
-student.last_name = "Thompson"
+student.first_name = 'Alan'
+student.last_name = 'Thompson'
 student.first_name
 student.last_name
 student.full_name
-student.add_course("Math")
-student.add_course("Programming")
-puts student.remove_course("New")
+student.add_course('Math')
+student.add_course('Programming')
+puts student.remove_course('New')
 puts student.list_course
