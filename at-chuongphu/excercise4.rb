9c1c1a7c9d60b2ce47fcbd59e9a12745b0f07c84
Chuong Phu <chuong.phu@asiantech.vn>
2018-08-05 08:20:13 +0700

    update-excercise-2

--- at-chuongphu/excercise4.rb
+++ at-chuongphu/excercise4.rb
@@ -1,20 +1,48 @@
 # ex1
-array = 'asianstech'.split(' ')
-array.each do |x|
-  puts array.index(x) if array.count(x) == 1
+def non_repeat(str)
+  hash = Hash.new (0)
+  arr = str.split('')
+  arr.each { |alpha| hash[alpha] += 1 }
+  num = hash.key(1)
+  num ? str.index(num) : -1 
 end
-# # ex2
-def sumarr(a)
-  check = a.all? { |x| x.is_a? Integer }
-  if check == true
-    puts a.reduce(0, :+)
-  else
-    puts 'nil'
+puts non_repeat('asiantech')
+
+# ex2
+class Array
+  def sum_arr
+    self.inject { |sum, a| sum + a }
   end
 end
-a = [1, 2, 4, 5]
-sumarr(a)
+p [1, 2, 4, 5].sum
+
 # ex4
-human_name = 'alan john'
-final = human_name.split.map(&:capitalize).join(' ')
-puts final
+class StringFormat
+  def human_name(str)
+    return unless str.is_a?(String)
+    @str = str
+    str = []
+    @str.strip!
+    arr = @str.split(' ')
+    arr.each do |i|
+      i.gsub!(/[^a-zA-Z]/, '')
+      if only_letter?(i)
+        i.capitalize!
+        str.push(i)
+      end
+    end
+    str.join('')
+  end
+
+  def uniq(str)
+    arr = str.split('')
+    arr.uniq.join('')
+  end
+
+  def only_letter?(str)
+    str[/[a-zA-Z]+/] == str if str.is_a?(String)
+  end
+end
+
+str = StringFormat.new
+p str.human_name('kayle _johnson')
